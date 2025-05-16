36c10dde77ca5a15aa879309a6400745df8afbbb
Ulises Hernández Calzadillas <ulises.hernandez.calzadillas@gmail.com>
2025-05-16 16:51:21 +0200

    fix: open CORS

--- personal_qa_rag/api/app.py
+++ personal_qa_rag/api/app.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # or ["*"] to allow all
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
